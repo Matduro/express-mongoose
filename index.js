7059c5bff02ff07edea19e17485e1580c23b274c
Mathieu <mathieurousseau123@gmail.com>
2021-07-13 15:15:44 -0700

    install method-override and implement put request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 
@@ -20,7 +21,9 @@ mongoose
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
@@ -47,7 +50,10 @@ app.get("/products/:id/edit", async (req, res) => {
   res.render("products/edit", { product });
 });
 
-app.put("/products/:id", async (req, res) => {});
+app.put("/products/:id", async (req, res) => {
+  console.log(req.body);
+  res.send("PUT");
+});
 
 app.post("/products", async (req, res) => {
   // console.log(req.body);
