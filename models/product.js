aa699b032ea4ecb3790020c065e33d4cf202c7a2
Mathieu <mathieurousseau123@gmail.com>
2021-07-13 09:33:46 -0700

    update seeds with additional data points

--- models/product.js
+++ models/product.js
@@ -12,7 +12,7 @@ const productSchema = new mongoose.Schema({
   },
   category: {
     type: String,
-    lowercase: true
+    lowercase: true,
     enum: ["fruit", "vegetable", "dairy"],
   },
 });
