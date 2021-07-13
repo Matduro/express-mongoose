02bb6a9c516ea87eec4fe7579c283be7e95002ae
Mathieu <mathieurousseau123@gmail.com>
2021-07-13 09:08:06 -0700

    set up product page

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/shopApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!");
+  })
+  .catch((error) => {
+    console.log("OH NO THE MONGO CONNECTION DID NOT WORK!!");
+    console.log(error);
+  });
 
 app.set("views", path.join(__dirname, "views"));
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/dog", (req, res) => {
   res.send("WOOF!");
