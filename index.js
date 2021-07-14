85b0923d55fd994aa16e3750d75958d37b21f6c6
Mathieu <mathieurousseau123@gmail.com>
2021-07-14 13:09:41 -0700

    Implement delete feature for products

--- index.js
+++ index.js
@@ -77,6 +77,12 @@ app.post("/products", async (req, res) => {
   res.redirect(`/products/${newProduct._id}`);
 });
 
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
 app.listen(3000, () => {
   console.log("APP IS LISTENING ON PORT 3000!");
 });
